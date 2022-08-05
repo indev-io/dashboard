451bfae7f1bd76c2be3cf8b270fb69f1bfafbf8b
ImmortalCount <devnaut@gmail.com>
2022-08-05 12:41:00 -0700

    structure intialized

--- src/App.js
+++ src/App.js
@@ -1,10 +1,59 @@
-import React from 'react'
+import React, {useEffect} from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { FiSettings } from 'react-icons/fi'
+import { TooltipComponent } from '@syncfusion/ej2-react-popups'
+import {Navbar, Footer, Sidebar, ThemeSettings} from './components'
+import {Ecommerce, Orders, Calendar, Employees, Stacked, Pyramid, Customers, Kanban, Area, Bar, Pie, Financial, ColorPicker, ColorMapping, Editor } from './pages'
 
 import './App.css'
 
 const App = () => {
+  const activeMenu = true
   return (
-    <h1 className='underline text-3xl'>App</h1>
+    <>
+    <BrowserRouter>
+    <div className='flex relative dark:bg-main-dark-bg'>
+      <div className='fixed right-4 bottom-4' style={{ zIndex: '1000'}}>
+        <TooltipComponent content='Settings' position='Top'>
+          <button type='button' className='text-3xl p-3 hover:drop-shadow-xl hover:bg-light-gray text-white' style={{background: 'blue', borderRadius: '50%'}}>
+            <FiSettings/>
+          </button>
+        </TooltipComponent>
+      </div>
+      {activeMenu ? (
+        <div className='w-72 fixed sidebar dark:bg-secondary-dark-bg bg-white'>
+          Sidebar
+        </div>
+      ): (
+        <div className='w-0 dark:bg-secondary-dark-bg'>
+          Sidebar w-0
+        </div>
+      )}
+      <div className={activeMenu ? 'dark:bg-main-bg bg-main-bg min-h-screen md:ml-72 w-full' : 'dark:bg-main-bg bg-main-bg min-h-screen w-full flex-2'}>
+        <Routes>
+          <Route path='/' element='Ecommerce' />
+          <Route path='/ecommerce' element='Ecommerce'/>
+          <Route path='/orders' element='Orders'/>
+          <Route path='/employees' element='Employees'/>
+          <Route path='/kanban' element='kanban'/>
+          <Route path='/editor' element='editor'/>
+          <Route path='/calendar' element='calendar'/>
+          <Route path='/color-picker' element='color-picker'/>
+          <Route path='/line' element='line'/>
+          <Route path='/area' element='area'/>
+          <Route path='/bar' element='bar'/>
+          <Route path='/pie' element='pie'/>
+          <Route path='/financial' element='financial'/>
+          <Route path='/color-mapping' element='color-mapping'/>
+          <Route path='/pyramid-chart' element='pyramid-chart'/>
+          <Route path='/stacked-chart' element='stacked-chart'/>
+        </Routes>
+      </div>
+    </div>
+    </BrowserRouter>
+       
+    </>
+ 
   )
 }
 
---
+++ src/components/Charts/SparkLine.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const SparkLine = () => {
+  return (
+    <div>Sparkline</div>
+  )
+}
+
+export default SparkLine
